5fd7c6964393e0544305d3b185c56ef935f32a2e
pramestya <54470282+pramestya@users.noreply.github.com>
2020-10-10 14:31:08 +0700

    Add files via upload

---
+++ comments.java
@@ -0,0 +1,8 @@
+public class comments {
+	public static void main (String[] args){
+		System.out.println("Hello World aja lagi"); //ini langsung print perbaris
+		System.out.print("gapapa lah ya . ."); // ini print biasa
+		/*komen panjang
+		*/
+	}
+}

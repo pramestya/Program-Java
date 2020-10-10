e172294c36148cceb51f648f8491af8c8ecf99c3
pramestya <54470282+pramestya@users.noreply.github.com>
2020-10-10 14:22:11 +0700

    Update and rename tigasatu.java to aSimpleJavaProgram.java

--- aSimpleJavaProgram.java
+++ aSimpleJavaProgram.java
@@ -1,4 +1,4 @@
-public class tigasatu {
+public class aSimpleJavaProgram {
 	public static void main (String[] args){
 		System.out.println("Hello World aja lagi");
 		System.out.print("gapapa lah ya . .");

a082f58d45c77d6f1b9dc06480fd2f69835e1b5c
pramestya <54470282+pramestya@users.noreply.github.com>
2020-10-11 15:29:16 +0700

    Add files via upload

---
+++ FloatingPointTypes.java
@@ -0,0 +1,13 @@
+public class FloatingPointTypes{
+	public static void main(String[] args){
+	
+	float datFloat;
+	double datDouble;
+	
+	datFloat = -2.30000045f; 
+	datDouble = -1.2131423861347512; 
+	
+	System.out.println(datFloat);
+	System.out.println(datDouble);
+	}
+}
